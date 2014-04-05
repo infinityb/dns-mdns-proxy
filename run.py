957619bb005b48ab452042cebd4b962415e0e187
Stacey Ell <stacey.ell@gmail.com>
2014-04-04 18:59:43 -0600

    domain name can be specified on the command-line

--- run.py
+++ run.py
@@ -1,9 +1,29 @@
+import os
+import sys
+import pwd
+import grp
 import gevent
 import untitled
 
-listener = untitled.ZeroConfListener(interface='br0')
 
-upstream = untitled.ZeroConfUpstream('office.corp.yasashiisyndicate.org', listener)
+def drop_privileges(uid_name='nobody', gid_name='nogroup'):
+    if os.getuid() != 0:
+        return
+
+    running_uid = pwd.getpwnam(uid_name).pw_uid
+    running_gid = grp.getgrnam(gid_name).gr_gid
+    os.setgroups([])
+    os.setgid(running_gid)
+    os.setuid(running_uid)
+    os.umask(077)
+
+
+listener = untitled.ZeroConfListener(interface='br0')
+upstream = untitled.ZeroConfUpstream(sys.argv[1], listener)
 adns = untitled.AuthoritativeDnsServer(upstream)
 
-gevent.sleep(400)
+
+drop_privileges()
+
+while True:
+    gevent.sleep(400)
